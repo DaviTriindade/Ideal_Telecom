c8861bd2b245a0ebe51676c94def7536c8cec0a5
Davi Trindade <hantcok@gmail.com>
2025-02-06 20:52:29 -0300

    Atualizando configurações para exportação estática

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', 
+  basePath: '/Site_Ideal_Telecom',  
+  trailingSlash: true,  
 };
 
 export default nextConfig;
