8903d2fd444e70e383bdfbd0f81a32bf0fb23b2e
Davi Trindade <hantcok@gmail.com>
2025-02-06 21:29:04 -0300

    correcoes

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export', 
-  basePath: '/Site_Ideal_Telecom',  
-  trailingSlash: true,  
+  basePath: '/Site_Ideal_Telecom', 
+  trailingSlash: true, 
 };
 
 export default nextConfig;
