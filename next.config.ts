47060bc598d467c575fb68114eb705781f89809f
Davi Trindade <hantcok@gmail.com>
2025-02-06 21:33:11 -0300

    Salvando mudanças antes do pull

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export', 
-  basePath: '/Site_Ideal_Telecom', 
+  basePath: '/Ideal_Telecom', 
   trailingSlash: true, 
 };
 
